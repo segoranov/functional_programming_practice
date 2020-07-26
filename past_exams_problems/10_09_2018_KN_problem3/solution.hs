195a02ca54162ef0623ef4b8e2baa417147d7c9e
Stiliyan Goranov <stiliyan.goranov@opencode.com>
2020-07-26 13:55:35 +0300

    Solve 10_08_2018 problem in haskell

---
+++ past_exams_problems/10_09_2018_KN_problem3/solution.hs
@@ -0,0 +1,13 @@
+selectList :: Foldable t => t a -> t a -> t a
+selectList l1 l2 = if length l1 >= length l2 then l1 else l2
+
+sumMaxRoots :: (Eq a1, Num a2, Num a1) => (a2 -> a1) -> [[a2]] -> a2
+sumMaxRoots f xss =
+    sum
+        (foldl
+            selectList
+            []
+            (map (\xs -> [x | x <- xs, f x == 0]) xss))
+
+-- *Main> sumMaxRoots (\x -> x^3 - x) [ [1, 2, 3], [-1, 0, 5], [1, 4, -1] ]
+-- -1
