de174e9b69ca647eb015f2c25892d188cd5bb428
Stiliyan Goranov <segoranov@gmail.com>
2020-08-02 13:51:40 +0300

    Solve problem from past exam in haskell - permutations of list

---
+++ past_exams_problems/11_09_2014_KN_problem7/permutations.hs
@@ -0,0 +1,15 @@
+import Data.List hiding (permutations)
+
+-- permutations [1,2,3] -> [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+putEverywhere :: a -> [a] -> [[a]]
+putEverywhere x xs = [insertAt n x xs | n <- [0..length xs]]
+    where insertAt n x xs = take n xs ++ [x] ++ drop n xs
+
+permutations :: [a] -> [[a]]
+permutations [x] = [[x]]
+permutations (x:xs) = foldl (++) [] [putEverywhere x permsTail | permsTail <- permutations xs]
+
+main = do
+    print (permutations [1,2, 3])
+    -- print (putEverywhere 1 [2,3,4])
