60f5aa1900cb51381fb8d1e21855a701eb0ad643
Stiliyan Goranov <segoranov@gmail.com>
2020-08-04 18:58:02 +0300

    Add TODO

--- past_exams_problems/15_07_2014_KN_problem7/solution.hs
+++ past_exams_problems/15_07_2014_KN_problem7/solution.hs
@@ -6,5 +6,7 @@ totalMin fs =
         (map (\f -> (f, f 0)) fs)
     )
 
+-- TODO: Solve b)
+
 main = do
   print (totalMin [(\x -> x + 2), (\x -> x - 100), (\x -> x + 3)] $ 100) -- tests a), prints 0 correctly.
